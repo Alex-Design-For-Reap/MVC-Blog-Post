b06b35ec93612438594e8a4e8081633fef99ac8c
design4reap <design4reap@gmail.com>
2024-07-02 23:41:32 +1000

    adding logout feature to it, creating logout.js to handle the destroy method

---
+++ public/js/logout.js
@@ -0,0 +1,19 @@
+const logout = async () => {
+    // Send a POST request to destroy the session on the back end
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+    // sessionStorage.removeItem('accessToken')
+    sessionStorage.removeItem('user')
+    
+    if (response.ok) {
+        // If successfully logged out, redirect to the homepage
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
+// document.querySelector('#logout').addEventListener('click', logout);
