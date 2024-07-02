24c6e456b373c6ee40e9bff1f64a552ec22868b5
design4reap <design4reap@gmail.com>
2024-07-02 22:40:54 +1000

    working on login/signup/logout routes for users, creating dashboard.handlebars to send the user after successful login, creating login.js file for handling login form

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,33 +1,46 @@
 // controllers/api/userRoutes.js
 const router = require('express').Router();
 const { User } = require('../../models');
+const withAuth = require('../../utils/auth');
 
+
+// create a new user
 router.post('/signup', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const newUser = await User.create(
+      {
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+      }
+    );
+
     req.session.save(() => {
-      req.session.user_id = userData.id;
+      req.session.user_id = newUser.id;
       req.session.logged_in = true;
-      res.status(200).json(userData);
+      res.status(200).json(newUser);
     });
   } catch (err) {
+    console.error(err);
     res.status(400).json(err);
   }
 });
 
+
+// login a user
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { username: req.body.username } });
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
-      res.status(400).json({ message: 'Incorrect username or password, please try again' });
+      res.status(400).json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
     const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect username or password, please try again' });
+      res.status(400).json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
@@ -38,10 +51,12 @@ router.post('/login', async (req, res) => {
     });
 
   } catch (err) {
+    console.error(err);
     res.status(400).json(err);
   }
 });
 
+// logout a user
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
---
+++ public/js/login.js
@@ -0,0 +1,38 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // Collect values from the login form
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+  
+    if (email && password) {
+    try {
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      const data = await response.json();
+  
+      if (response.ok) {
+        // sessionStorage.setItem('accessToken', JSON.stringify(data.accessToken));
+        sessionStorage.setItem('user', JSON.stringify(data.user));
+        document.location.replace('/dashboard');
+      } else {
+        alert('Login or password is incorrect. Please try again.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      alert('An error occurred. Please try again later.');
+    }
+  } else {
+    alert('Please fill in both fields.');
+  }
+  };  
+  
+  
+  document
+      .querySelector('#login-form')
+      .addEventListener('submit', loginFormHandler);
