574a1c03109468c0568b9e10a1e4395cbb4bd5f2
design4reap <design4reap@gmail.com>
2024-07-04 23:31:54 +1000

    handling render issues

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,16 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DATABASE_URL || process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
   host: process.env.DB_HOST,
   dialect: 'postgres',
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      // rejectUnauthorized: false,
+    }
+  }
 });
 
 module.exports = sequelize;
