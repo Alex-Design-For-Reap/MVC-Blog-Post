1482af6b9022d9c470f2c88996f2c9db0541f74e
design4reap <design4reap@gmail.com>
2024-07-05 21:26:08 +1000

    fix SSL configuration for production

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,10 @@ require("dotenv").config();
 let sequelize;
 
 if (process.env.NODE_ENV === 'production') {
-  //sequelize = new Sequelize(process.env.DB_URL);
   sequelize = new Sequelize(
-    process.env.DATABASE_URL ||
-    process.env.DB_NAME,
+    process.env.DATABASE_URL || process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD, // Ensure this matches your .env file
+    process.env.DB_PASSWORD,
     {
       host: process.env.DB_HOST,
       dialect: "postgres",
@@ -19,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
       dialectOptions: {
         "ssl": {
           "require": true,
-          // "rejectUnauthorized": false
+          "rejectUnauthorized": false
         }
       }
     }
@@ -28,10 +26,11 @@ if (process.env.NODE_ENV === 'production') {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD, // Ensure this matches your .env file
+    process.env.DB_PASSWORD,
     {
-      host: "localhost",
+      host: process.env.DB_HOST || "localhost",
       dialect: "postgres",
+      port: process.env.DB_PORT || 5432,
     }
   );
 }
