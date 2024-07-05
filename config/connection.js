f1e900dc96c80dcd824411682b151e99efe0f6ab
design4reap <design4reap@gmail.com>
2024-07-05 22:43:19 +1000

    debuging render deployment

--- config/connection.js
+++ config/connection.js
@@ -39,21 +39,3 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 module.exports = sequelize;
-
-
-// const Sequelize = require('sequelize');
-// require('dotenv').config();
-
-// const sequelize = new Sequelize(process.env.DATABASE_URL || process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
-//   host: process.env.DB_HOST,
-//   dialect: 'postgres',
-//   port: process.env.DB_PORT,
-//   // dialectOptions: {
-//   //   ssl: {
-//   //     require: true,
-//   //     // rejectUnauthorized: false,
-//   //   }
-//   // }
-// });
-
-// module.exports = sequelize;
--- seeds/index.js
+++ seeds/index.js
@@ -5,9 +5,13 @@ const seedComments = require("./seedComments");
 
 const seedAll = async () => {
   try {
-    await sequelize.sync({ force: true });
+    console.log("Syncing database...");
+    await sequelize.sync({ force: true }); // Syncs the models and creates the schema
+    console.log("Seeding users...");
     await seedUsers();
+    console.log("Seeding posts...");
     await seedPosts();
+    console.log("Seeding comments...");
     await seedComments();
     console.log("Database seeded successfully!");
   } catch (error) {
